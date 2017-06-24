c03f2139b75a16f9d139cb14734f12d507590a55
mauricio.togneri <mauricio.togneri@gmail.com>
2017-06-25 01:03:23 +0200

    Updated library version

--- src/main/java/com/mauriciotogneri/javautils/EnumValue.java
+++ src/main/java/com/mauriciotogneri/javautils/EnumValue.java
@@ -2,11 +2,37 @@ package com.mauriciotogneri.javautils;
 
 public class EnumValue
 {
-    public static <T extends Enum<T>> T from(Class<T> clazz, String text, T defaultValue)
+    public enum Mode
     {
+        NORMAL,
+        UPPERCASE,
+        LOWERCASE;
+
+        public String format(String text)
+        {
+            if (text != null)
+            {
+                if (this == UPPERCASE)
+                {
+                    return text.toUpperCase();
+                }
+                else if (this == LOWERCASE)
+                {
+                    return text.toLowerCase();
+                }
+            }
+
+            return text;
+        }
+    }
+
+    public static <T extends Enum<T>> T from(Class<T> clazz, String text, T defaultValue, Mode mode)
+    {
+        String input = mode.format(text);
+
         for (T value : clazz.getEnumConstants())
         {
-            if (value.toString().equals(text))
+            if (value.toString().equals(input))
             {
                 return value;
             }
@@ -15,9 +41,19 @@ public class EnumValue
         return defaultValue;
     }
 
+    public static <T extends Enum<T>> T from(Class<T> clazz, String text, T defaultValue)
+    {
+        return from(clazz, text, defaultValue, Mode.NORMAL);
+    }
+
+    public static <T extends Enum<T>> T from(Class<T> clazz, String text, Mode mode)
+    {
+        return from(clazz, text, null, mode);
+    }
+
     public static <T extends Enum<T>> T from(Class<T> clazz, String text)
     {
-        return from(clazz, text, null);
+        return from(clazz, text, null, Mode.NORMAL);
     }
 
     public static <T extends Enum> T random(Class<T> clazz)
